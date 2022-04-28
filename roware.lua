35735e62f93d898fae43bad5adc109a7d4334c29
https0x1 <46610385+NotAVeryGoodName@users.noreply.github.com>
2022-04-27 23:25:04 -0700

    Create roware.lua

---
+++ roware.lua
@@ -0,0 +1,3 @@
+_G.Fling = false -- switch to true for fling
+_G.BotReanim = false -- enable in setts
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TypicallyAUser/random-stuff/main/rowareyesmane.lua",true))()
