872b028a41d4713e18d86b71fe85a1fa4ebd235b
https0x1 <46610385+NotAVeryGoodName@users.noreply.github.com>
2022-05-08 03:57:03 -0700

    Update sound.lua

--- sound.lua
+++ sound.lua
@@ -1,5 +1,5 @@
 local newsound = Instance.new("Sound",workspace)
 newsound.Name = "earbleed"
-newsound.SoundId = "rbxassetid://9576175596"
+newsound.SoundId = "rbxassetid://9576364355"
 newsound:Play()
-conntect(sound) 
+conntect(sound)
