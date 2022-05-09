42e8ebec3e210e57ae40d770b9a78542840e22a1
https0x1 <46610385+NotAVeryGoodName@users.noreply.github.com>
2022-05-08 19:14:24 -0700

    Create raf.lua

---
+++ raf.lua
@@ -0,0 +1,7 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+
+_G.RedGUI = false
+_G.Theme = "Jester" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/raiseafloppa.lua"))()
